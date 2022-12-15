2a296369176a9616d097b98a5f747f1bab82330e
1337cod3r <87788111+1337cod3r@users.noreply.github.com>
2022-12-15 23:53:02 +0200

    Create preset.cpp

---
+++ preset.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iterator>
+#include <set>
+#include <map>
+#include <math.h>
+#include <iomanip>
+#include <sstream>
+#include <cmath>
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+
+ull mod = 1000000007;
+
+
+int main(){
+
+	cin >> 
+
+
+
+	cout << /*setprecision() <<*/ 
+
+	return 0;
+}
